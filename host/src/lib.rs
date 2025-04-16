18d67e8e9eb3e4bdc0e451a94df396c377629569
wkrokosz <w.krasnicki@cent.uw.edu.pl>
2025-04-16 19:21:14 +0200

    Tokio runtime start

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -1,11 +1,18 @@
+use std::sync::OnceLock;
+
 use pyo3::prelude::*;
 
 mod servo;
 
 use servo::ServoClient;
+use tokio::runtime::Runtime;
+
+static RT: OnceLock<Runtime> = OnceLock::new();
 
 #[pymodule]
 fn rustpill_clients(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    RT.get_or_init(|| Runtime::new().expect("Failed to create Tokio runtime"));
+
     m.add_class::<ServoClient>()?;
     Ok(())
 }
