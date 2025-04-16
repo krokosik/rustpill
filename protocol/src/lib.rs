3c5b0e74e40e3ee0c67da4eaeb26b22ca821e97a
wkrokosz <w.krasnicki@cent.uw.edu.pl>
2025-04-16 12:29:29 +0200

    Add protocol example schemas

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![cfg_attr(not(feature = "use-std"), no_std)]
+
+use postcard_rpc::{TopicDirection, endpoints, topics};
+use postcard_schema::Schema;
+use serde::{Deserialize, Serialize};
+
+// ---
+
+pub type SingleLedSetResult = Result<(), BadPositionError>;
+pub type AllLedArray = [Rgb8; 24];
+
+endpoints! {
+    list = ENDPOINT_LIST;
+    omit_std = true;
+    | EndpointTy                | RequestTy     | ResponseTy            | Path              |
+    | ----------                | ---------     | ----------            | ----              |
+    | PingEndpoint              | u32           | u32                   | "ping"            |
+    | GetUniqueIdEndpoint       | ()            | u64                   | "unique_id/get"   |
+    | SetSingleLedEndpoint      | SingleLed     | SingleLedSetResult    | "led/set_one"     |
+    | SetAllLedEndpoint         | AllLedArray   | ()                    | "led/set_all"     |
+    | StartAccelerationEndpoint | StartAccel    | ()                    | "accel/start"     |
+    | StopAccelerationEndpoint  | ()            | bool                  | "accel/stop"      |
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+topics! {
+    list = TOPICS_IN_LIST;
+    direction = TopicDirection::ToServer;
+    | TopicTy                   | MessageTy     | Path              |
+    | -------                   | ---------     | ----              |
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+topics! {
+    list = TOPICS_OUT_LIST;
+    direction = TopicDirection::ToClient;
+    | TopicTy                   | MessageTy     | Path              | Cfg                           |
+    | -------                   | ---------     | ----              | ---                           |
+    | AccelTopic                | Acceleration  | "accel/data"      |                               |
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq)]
+pub struct SingleLed {
+    pub position: u32,
+    pub rgb: Rgb8,
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq, Copy, Clone)]
+pub struct Rgb8 {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq)]
+pub struct BadPositionError;
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq)]
+pub struct Acceleration {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq)]
+pub enum AccelRange {
+    G2,
+    G4,
+    G8,
+    G16,
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug, PartialEq)]
+pub struct StartAccel {
+    pub interval_ms: u32,
+    pub range: AccelRange,
 }
