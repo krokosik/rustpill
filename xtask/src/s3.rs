09bbbe13a6c7dd3a912f4c71001335bdc3c8df9f
krokosik <krokosik@pm.me>
2025-06-02 19:18:10 +0200

    Allow using public S3 bucket

--- xtask/src/s3.rs
+++ xtask/src/s3.rs
@@ -3,28 +3,28 @@ use std::{env, error::Error, path::Path};
 const MINIO_BUCKET: &str = "rustpill-firmwares";
 
 pub fn get_bucket() -> Result<Box<s3::Bucket>, Box<dyn Error>> {
-    let minio_endpoint = env::var("MINIO_ENDPOINT").expect("MINIO_ENDPOINT not set in .env");
-    let access_key_id =
-        env::var("MINIO_ACCESS_KEY_ID").expect("MINIO_ACCESS_KEY_ID not set in .env");
-    let secret_access_key =
-        env::var("MINIO_SECRET_ACCESS_KEY").expect("MINIO_SECRET_ACCESS_KEY not set in .env");
+    let minio_endpoint = env::var("MINIO_ENDPOINT").unwrap_or("https://s3.qodl.eu".to_string());
+    let access_key_id = env::var("MINIO_ACCESS_KEY_ID").ok();
+    let secret_access_key = env::var("MINIO_SECRET_ACCESS_KEY").ok();
 
     let region = s3::Region::Custom {
         region: "eu-central-1".to_string(),
         endpoint: minio_endpoint,
     };
-    let creds = s3::creds::Credentials::new(
-        Some(&access_key_id),
-        Some(&secret_access_key),
-        None,
-        None,
-        None,
-    )?;
-    let mut bucket = s3::Bucket::new(MINIO_BUCKET, region, creds)?;
+
+    let mut bucket = if access_key_id.is_none() || secret_access_key.is_none() {
+        Box::new(s3::Bucket::new_public(MINIO_BUCKET, region)?)
+    } else {
+        let creds = s3::creds::Credentials::new(
+            access_key_id.as_deref(),
+            secret_access_key.as_deref(),
+            None,
+            None,
+            None,
+        )?;
+        s3::Bucket::new(MINIO_BUCKET, region, creds)?
+    };
     bucket.set_path_style();
-    if !bucket.exists()? {
-        return Err(format!("Bucket {} does not exist", MINIO_BUCKET).into());
-    }
     Ok(bucket)
 }
 
