1fc6244d5032f2a2e81de9e0b963b0280777c6ad
krokosik <krokosik@pm.me>
2025-06-02 17:44:12 +0200

    Create a static method for flashing servo firmware

--- host/src/servo.rs
+++ host/src/servo.rs
@@ -5,7 +5,10 @@ use pyo3::prelude::*;
 use pyo3_stub_gen::derive::*;
 use std::convert::Infallible;
 
-use crate::common::{BoardError, connect_to_board};
+use crate::{
+    common::{BoardError, connect_to_board},
+    flash::flash_binary,
+};
 
 const STM32_PWM_RESOLUTION_BITS: u8 = 16;
 
@@ -33,6 +36,12 @@ impl ServoClient {
         log::info!("Servo config: {:?}", config);
 
         Ok(Self { client, config })
+    }
+
+    #[staticmethod]
+    fn flash() -> PyResult<()> {
+        flash_binary("servo")?;
+        Ok(())
     }
 
     /// Close the connection to the board.
